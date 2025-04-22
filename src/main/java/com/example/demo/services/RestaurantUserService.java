d9d7dedcffa6bdd87c7b317564becef1c01d56e8
ValentinoGianuzzo <valentino.gianuzzo@ing.austral.edu.ar>
2025-04-22 09:58:23 -0300

    Corregir x2

--- src/main/java/com/example/demo/services/RestaurantUserService.java
+++ src/main/java/com/example/demo/services/RestaurantUserService.java
@@ -36,7 +36,7 @@ public class RestaurantUserService {
         );
     }
 
-    public Optional<RestaurantUserDTO> getRestaurantUserById(UUID id) {
+    public RestaurantUser getRestaurantUserById(UUID id) {
         return restaurantUserRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
